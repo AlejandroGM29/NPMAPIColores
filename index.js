81761fbe3360294fec7501297912442fc1b262d7
alejandrogm29 <alejandroGMA29@hotmail.com>
2023-08-30 18:52:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const tiposDePokemon = {
+    "agua": "blue",
+    "fuego": "red",
+    "planta": "green",
+    "electrico": "yellow",
+    "lucha": "orange",
+    "psiquico": "purple",
+    "tierra": "brown",
+    "volador": "lightblue",
+    "hielo": "white",
+    "veneno": "purple",
+    "bicho": "lightgreen",
+    "normal": "gray"
+};
+
+function obtenerColorTipo(tipo){
+    return tiposDePokemon(tipo);
+}
+console.log(module)
+
+module.exports.obtenerColorTipo = obtenerColorTipo;
+
+console.log(module)
+
